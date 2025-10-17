d9eb10543c4d4bf2dfccb196042e2b98fe162e17
Peter Kakuru Bingwa <pkakuru@gmail.com>
2025-10-17 09:29:00 +0300

    Add test for addition function

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,5 @@
 import pytest
-from calculator import get_numbers
+from calculator import get_numbers, add_numbers
 from unittest.mock import patch
 
 
@@ -8,3 +8,9 @@ def test_get_numbers_with_invalid_input(mock_input):
     expected_output = [5.0, 10.0]
     result = get_numbers()
     assert result == expected_output
+
+
+def test_add_positive_numbers():
+    numbers = [1, 2, 3, 4.5]
+    expected_sum = 10.5
+    assert add_numbers(numbers) == expected_sum
